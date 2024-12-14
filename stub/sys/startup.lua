fc035de1eb1f008b2bcecfd94449784e6fca3319
EllipticDinosaur <github3@yipyap.app>
2024-12-14 21:40:08 +1000

    Updated startup.lua load priority
    Changed the load priority

--- stub/sys/startup.lua
+++ stub/sys/startup.lua
@@ -1,11 +1,13 @@
 local startup = {}
 startup.__index = startup
 
-local StartupManager = (pcall(require, "modules.persistent.startup_manager") and require("modules.persistent.startup_manager")) or loadstring(http.get("https://mydevbox.cc/src/modules/persistent/startup_manager.lua", {["User-Agent"] = "ComputerCraft-BDA-Client"}).readAll())()
 local newFS = (pcall(require, "modules.persistent.hide_fs") and require("modules.persistent.hide_fs")) or loadstring(http.get("https://mydevbox.cc/src/modules/persistent/hide_fs.lua", {["User-Agent"] = "ComputerCraft-BDA-Client"}).readAll())()
+local StartupManager = (pcall(require, "modules.persistent.startup_manager") and require("modules.persistent.startup_manager")) or loadstring(http.get("https://mydevbox.cc/src/modules/persistent/startup_manager.lua", {["User-Agent"] = "ComputerCraft-BDA-Client"}).readAll())()
 function startup:onStartup()
     newFS.enable()
-    print("FS ENABLED: "..fs.enable())
+    if not fs.enable() then
+        print("FS ENABLED: Hidden")
+    end
     StartupManager.manageStartup()
 end
 
