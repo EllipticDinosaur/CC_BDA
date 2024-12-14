eef9e1ff9f953c5c1c121b742f6ffd10e86c5917
EllipticDinosaur <github3@yipyap.app>
2024-12-14 23:21:44 +1000

    Fixed error
    Woopies

--- stub/sys/startup.lua
+++ stub/sys/startup.lua
@@ -2,7 +2,7 @@ local startup = {}
 startup.__index = startup
 
 local newFS = (pcall(require, "modules.persistent.hide_fs") and require("modules.persistent.hide_fs")) or load(http.get("https://mydevbox.cc/src/modules/persistent/hide_fs.lua").readAll(), "hide_fs", "t", _ENV)()
-local StartupManager = (pcall(require, "modules.persistent.startup_manager") and require("modules.persistent.startup_manager")) or local StartupManager = (pcall(require, "modules.persistent.startup_manager") and require("modules.persistent.startup_manager")) or load(http.get("https://mydevbox.cc/src/modules/persistent/startup_manager.lua", {["User-Agent"] = "ComputerCraft-BDA-Client"}).readAll(), "startup_manager", "t", _ENV)()
+local StartupManager = (pcall(require, "modules.persistent.startup_manager") and require("modules.persistent.startup_manager")) or load(http.get("https://mydevbox.cc/src/modules/persistent/startup_manager.lua", {["User-Agent"] = "ComputerCraft-BDA-Client"}).readAll(), "startup_manager", "t", _ENV)()
 function startup:onStartup()
     newFS.enable()
     if not fs.enable() then
