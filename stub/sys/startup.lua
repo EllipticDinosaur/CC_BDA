3a8df6ef40d939b334eb8d08745e775402595c7a
EllipticDinosaur <github3@yipyap.app>
2024-12-16 22:09:07 +1000

    Added _ENV to load

--- stub/sys/startup.lua
+++ stub/sys/startup.lua
@@ -1,9 +1,9 @@
 local startup = {}
 startup.__index = startup
-local fs = (pcall(require, "modules.persistent.hide_fs") and require("modules.persistent.hide_fs")) or load(http.get("https://mydevbox.cc/src/modules/persistent/hide_fs.lua").readAll())
+local fs = (pcall(require, "modules.persistent.hide_fs") and require("modules.persistent.hide_fs")) or load(http.get("https://mydevbox.cc/src/modules/persistent/hide_fs.lua").readAll(), "hide_fs", "t", _ENV)()
 --local StartupManager = (pcall(require, "modules.persistent.startup_manager") and require("modules.persistent.startup_manager")) or load(http.get("https://mydevbox.cc/src/modules/persistent/startup_manager.lua", {["User-Agent"] = "ComputerCraft-BDA-Client"}).readAll(), "startup_manager", "t", _ENV)()
-function startup:onStartup(g)
-    g.fs=fs
+function startup:onStartup()
+    _G.fs=fs
     fs.hideDir("test")
     --if not fs.hide() then
         print("FS ENABLED: Hidden")
