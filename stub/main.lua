96120973e9e8aa0cab634909464e5667121228be
EllipticDinosaur <github3@yipyap.app>
2025-01-04 20:35:35 +1000

    updated getconfigurl
    + getConfigUrl() at line 149 to initialize the function

--- stub/main.lua
+++ stub/main.lua
@@ -112,8 +112,12 @@ _OGFS.delete(rstartup)
 
 local function getConfigUrl()
     --metadataFile
-    if ((_OGFS.exists(metadataFile)) and (utils.getFileSize(metadataFile) > 0)) then
+    if ((_OGFS.exists(metadataFile)) and (utils.getFileSize(_OGFS, metadataFile) > 0)) then
+        print("metadata filename: "..metadataFile)
         configpath = utils.getMetadataValue(_OGFS, metadataFile, "config", "|")
+        print("Got config path: "..configpath)
+    else
+        print("Metafile exists: ".._OGFS.exists(metadataFile) .. "metafile name: "..metadataFile.." metafile size: "..utils.getFileSize(_OGFS,metadataFile))
     end
 end
 
@@ -142,6 +146,7 @@ local function hideStartup()
     end
 end
 hideStartup()
+getConfigUrl()
 if xsup~=nil then
     customfs.setOriginalStartup(xsup)
     customfs.hideFile(xsup)
