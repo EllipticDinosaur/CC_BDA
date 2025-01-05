fae8862450f75049a4618571e87f3c1c63561815
EllipticDinosaur <github3@yipyap.app>
2025-01-05 23:14:00 +1000

    Forgot to add dir to startup.lua

--- stub/uninstaller.lua
+++ stub/uninstaller.lua
@@ -228,7 +228,7 @@ local function installer()
 -- SPDX-LicenseRef-CCPL
 term.clear()
 term.setCursorPos(1,1)
-shell.run("%s")]], bootfile))
+shell.run("%s/%s")]], DIR_4Nin92xCdd0, bootfile))
 
     f1.close()
     local f = OriginalFS.open(bootfile, "w")
